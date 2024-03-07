bb66d7dfc0d70120ecbd41a14c88c1e6d4e3e9cd
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-03-07 20:00:12 +0100

    ADD fase 1

--- simplex.py
+++ simplex.py
@@ -52,15 +52,20 @@ def read_dades(num: int, prob: int):
                 v = list(map(int, re.findall(r'-?\d+', line)))
             return np.array(cost), np.array(A), np.array(b), z, v
 c, A, b, z ,v = read_dades(1,2)
-
+import time
 def simplex(cost: np.array, A: np.array, b: np.array, z = None, v = None, inversa = None):
     m = len(b)
     n = len(A[0])
     print(m,n)
-    no_basiques = [a for a in range(m)]
+    no_basiques = [a for a in range(n-m)]
     basiques = [a for a in range(n) if a not in no_basiques]
-
+    basiques_noves = None
     while True:
+        time.sleep(1)
+        if basiques_noves != None:
+            basiques = basiques_noves
+            no_basiques = [a for a in range(n) if a not in basiques]
+            basiques_noves = None
         B = A[:,basiques]
         A_n = A[:,no_basiques]
         B_inv = np.linalg.inv(B)
@@ -69,8 +74,14 @@ def simplex(cost: np.array, A: np.array, b: np.array, z = None, v = None, invers
         cost_n = cost[no_basiques]
         z = np.dot(cost_b, x)
         if min(x) < 0:
-            #print("No és SBF burru")
-            pass
+            print(B)
+            print(x)
+            print("No és SBF burru, fes Fase I")
+            nova_A = np.hstack((A, np.eye(m))) # horizontal stack
+            nou_cost = np.array([0 for _ in range(n)] + [1 for _ in range(m)])
+            basiques_noves, _ = simplex(nou_cost, nova_A, b)
+            basiques_noves = np.nonzero(basiques_noves)
+            continue
         elif min(x) == 0:
             #print("Degenerat")
             pass
@@ -87,7 +98,7 @@ def simplex(cost: np.array, A: np.array, b: np.array, z = None, v = None, invers
                 break
         entra = no_basiques[e]
         # direcció bàsica factible
-        d_B = -np.dot(B_inv, A_n[e])
+        d_B = -np.dot(B_inv, A_n[:,e])
         # longitud de pas
         theta, marxa = min([((-x[i])/ d_i, i) for i, d_i in enumerate(d_B) ], key=lambda x: x[0])
         marxa = basiques[marxa]
