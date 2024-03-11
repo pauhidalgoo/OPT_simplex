bd9d959bd9d1d34a100a5c82e1216e16429a16dd
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-03-11 12:15:10 +0100

    ADD test with all the problems

--- simplex.py
+++ simplex.py
@@ -1,8 +1,8 @@
 import numpy as np
 import re
-# hola
+
 def read_dades(num: int, prob: int):
-    with open("OPT_Simplex/OPT23-24_Datos práctica 1.txt", "r") as data:
+    with open("OPT23-24_Datos práctica 1.txt", "r") as data:
         line = data.readline()
         while line != None:
             while not("datos"+ str(num) in line.replace(" ", "") and "problemaPL" + str(prob) in line.replace(" ", "")):
@@ -44,14 +44,13 @@ def read_dades(num: int, prob: int):
                     break
             else:
                 line = data.readline()
-                print(line)
                 z = float(line)
                 line = data.readline()
                 line = data.readline()
                 line = data.readline()
                 v = list(map(int, re.findall(r'-?\d+', line)))
             return np.array(cost), np.array(A), np.array(b), z, v
-c, A, b, z ,v = read_dades(15,1)
+c, A, b, z ,v = read_dades(1,1)
 import time
 def simplex(cost: np.array, A: np.array, b: np.array, z = None, v = None, inversa = None):
     z_original = z
@@ -62,26 +61,33 @@ def simplex(cost: np.array, A: np.array, b: np.array, z = None, v = None, invers
     basiques_noves = None
     while True:
         if basiques_noves != None:
-            print("oleole")
-            print(basiques_noves)
             basiques = [a for a in range(n) if a in basiques_noves]
             no_basiques = [a for a in range(n) if a not in basiques]
             basiques_noves = None
         B = A[:,basiques]
         A_n = A[:,no_basiques]
-        B_inv = np.linalg.inv(B)
+        try:
+            B_inv = np.linalg.inv(B)
+        except:
+            #print("aui")
+            return None, None, None
         x = np.dot(B_inv, b)
         cost_b = cost[basiques]
         cost_n = cost[no_basiques]
         z = np.dot(cost_b, x)
         if min(x) < 0:
-            print("No és SBF burru, fes Fase I")
             nova_A = np.hstack((A, np.eye(m))) # horizontal stack
             nou_cost = np.array([0 for _ in range(n)] + [1 for _ in range(m)])
-            _, _, basiques_noves, _ = simplex(nou_cost, nova_A, b)
+            _, z_f1, basiques_noves, _= simplex(nou_cost, nova_A, b)
+            if z_f1 > 0:
+                #print("insatisfactible")
+                return x, None, None, None
             continue
         elif min(x) == 0:
             #print("Degenerat")
+            pass
+        elif min(x) < 0:
+            #print("algo va mal")
             pass
         # és optim?
         # és el MÉS ÒPTIM?
@@ -90,14 +96,18 @@ def simplex(cost: np.array, A: np.array, b: np.array, z = None, v = None, invers
             #print("No és òptim burru")
             pass
         else:
-            print("optim")
-            return x, z, basiques,z_original
+            #print("optim")
+            return x, z, basiques, z_original
         for e in range(len(r)):
             if r[e] < 0:
                 break
         entra = no_basiques[e]
         # direcció bàsica factible
         d_B = -np.dot(B_inv, A_n[:,e])
+
+        if min(d_B) >= 0:
+            #print("joquese")
+            return x, z, basiques, z_original
         # longitud de pas
         theta, marxa = min([((-x[i])/ d_i, i) for i, d_i in enumerate(d_B) if d_i < 0], key=lambda x: x[0])
         marxa = basiques[marxa]
@@ -105,7 +115,14 @@ def simplex(cost: np.array, A: np.array, b: np.array, z = None, v = None, invers
         no_basiques.remove(entra)
         no_basiques.append(marxa)
         no_basiques = sorted(no_basiques)
-        print(z)
+    
+for alumne in range(1, 45):
+    for problema in range(1, 5):
+        c, A, b, z ,v = read_dades(alumne,problema)
+        a = simplex(c, A, b, z, v)
+        if z != None:
+            result = f"{a[1]:.4f}" == f"{z:.4f}"
+            print(result, f"{a[1]:.4f}")
 
 def print_simplex(simplex):
     print("\n Solució: \n")
